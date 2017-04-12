cc58a5547ab6d1462a80a30c8abb31b789e32acf
vashkevich <7855862@gmail.com>
2017-04-12 17:17:11 +0300

    Update deploy.rb

--- cookbooks/jboss/recipes/deploy.rb
+++ cookbooks/jboss/recipes/deploy.rb
@@ -8,8 +8,8 @@
 
 remote_file "#{node['jboss']['install_folder']}/#{node['jboss']['app_dist_name']}" do
   source node['jboss']['app_repo']
-  owner node[:jboss][:jboss_user]
-  group node[:jboss][:jboss_group]
+  owner node[:jboss][:jb_user]
+  group node[:jboss][:jb_group]
   action :create_if_missing
   mode '0755'
 end
@@ -18,9 +18,15 @@ end
 
 execute 'unzip_test_app' do
   cwd node['jboss']['install_folder']
-  user node['jboss']['jb_user']
-# group node['jboss']['jb_group']
-# command "unzip -o #{node[:jboss][:app_dist_name]} -d #{node[:jboss][:app_deploy_folder]}"
-  not_if { ::File.exist?("#{node[:jboss][:app_deploy_folder]}/testweb")}
-  notifies :restart, 'service[jboss]', :immediately
+  command "unzip -o #{node[:jboss][:app_dist_name]} -d #{node[:jboss][:app_deploy_folder]}"
+  not_if { ::File.exist?("#{node[:jboss][:app_deploy_folder]}hudson")}
+end
+
+#******* Change config file for hudson
+
+template "#{node['jboss']['app_deploy_folder']}hudson/hudson.xml" do
+  source "xml.erb"
+  variables({
+    :engine => node[:jboss][:engine]
+  })
 end
